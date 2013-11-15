a0826772e48303b23448aa9e73f44b3e073be524
Fabian Vogelsteller <fabian.vogelsteller@tunedin.de>
2013-11-15 13:29:20 +0100

    Revert b30f23d..7a59fbd
    This rolls back to commit b30f23dac9fb6b8876d7f11661a0f52830dca8d8.

--- meteorCursorLimitTest.js
+++ meteorCursorLimitTest.js
@@ -1,14 +1,11 @@
 myCollection = new Meteor.Collection('myColl', {connection: null});
 
-mySecondCollection = new Meteor.Collection('mySecondColl', {connection: null});
-
 if (Meteor.isClient) {
 
 
 
   // set default limit
   Session.setDefault('limit', 10);
-  Session.setDefault('rerunHelper', _.uniqueId());
 
 
   // The cursor gets passed trough the helper to the grid template
@@ -17,16 +14,8 @@ if (Meteor.isClient) {
   };
 
   Template.main.events({
-    'click button.increase' : function () {
+    'click button' : function () {
       Session.set('limit', 20);
-    },
-    'click button.rerun' : function () {
-      // Session.set('rerunHelper', _.uniqueId());
-
-      // change the items data
-      _.each(mySecondCollection.find().fetch(), function(item){
-        mySecondCollection.update(item._id, {$set: { someValue: _.uniqueId() }});
-      });
     }
   });
 
@@ -46,29 +35,14 @@ if (Meteor.isClient) {
         myCollection.insert({
           itemId: 5
         });
-        mySecondCollection.insert({
-          itemId: 5
-        });
       }
 
   };
-  Template.grid.placeTemplate = function(itemTemplate){
-    return Template[itemTemplate].withData(this);
+
+
+  Template.grid.isRendered = function(){
+    console.log('List item rendered');
   };
-
-
-
-
-  Template.item.secondColl = function(){
-    return mySecondCollection.findOne({itemId:this.itemId});
-    // if(this.lastRerunId && this.lastRerunId !== Session.get('rerunHelper'))
-      // console.log('Rerun helper');
-    // this.lastRerunId = Session.get('rerunHelper');
-  };
-  Template.item.isCalled = function(){
-    console.log('List item helper called');
-  };
-
 
 
 }
