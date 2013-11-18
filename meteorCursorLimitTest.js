8141b615072000a6da0e41d0a08d6dbcd335461e
Fabian Vogelsteller <fabian.vogelsteller@tunedin.de>
2013-11-18 10:25:14 +0100

    changed to order type

--- meteorCursorLimitTest.js
+++ meteorCursorLimitTest.js
@@ -1,24 +1,32 @@
 myCollection = new Meteor.Collection('myColl', {connection: null});
 
-mySecondCollection = new Meteor.Collection('mySecondColl', {connection: null});
 
 if (Meteor.isClient) {
 
 
 
-  // set default limit
-  Session.setDefault('limit', 10);
+  // set default orderType
+  Session.setDefault('orderType', 'itemId');
   Session.setDefault('rerunHelper', _.uniqueId());
 
 
   // The cursor gets passed trough the helper to the grid template
   Template.main.theCursor = function () {
-    return myCollection.find({}, {limit: Session.get('limit')});
+
+    var order = Session.get('orderType');
+
+    if(order === 'itemId')
+      return myCollection.find({}, {sort: {'itemId': 1}});
+    if(order === 'name')
+      return myCollection.find({}, {sort: {'name': 1}});
   };
 
   Template.main.events({
-    'click button.increase' : function () {
-      Session.set('limit', Session.get('limit') + 10);
+    'click button.order' : function () {
+      if(Session.equals('orderType','itemId'))
+        Session.set('orderType', 'name');
+      else
+        Session.set('orderType', 'itemId');
     },
     'click button.rerun' : function () {
       Session.set('rerunHelper', _.uniqueId());
@@ -39,7 +47,8 @@ if (Meteor.isClient) {
       // fill the collection AFTER the template gets rendered
       for (var i = 0; i < 80; i++) {
         myCollection.insert({
-          itemId: i + 1
+          itemId: i + 1,
+          name: Math.random().toString(36).substring(7)
         });
       }
 
